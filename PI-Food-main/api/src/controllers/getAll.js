8d36c4515dbc6415b97e6b5d58bbfdfbfce07efd
H3rC4 <hernanenriquecaballero@gmail.com>
2023-06-06 16:38:01 +0200

    getRecepiById funcionando

--- PI-Food-main/api/src/controllers/getAll.js
+++ PI-Food-main/api/src/controllers/getAll.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
-const { API_KEY} = process.env;
-const axios = require('axios');
-
+// const { API_KEY} = process.env;
+// const axios = require('axios');
+const respuesta = require('./los100')
 
 module.exports = async (req,res)=>{
-   const response = (await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=100&addRecipeInformation=true`)).data;
-   console.log(response.results);
-   res.status(200).json(response.results)
+ 
+ 
+  res.status(200).json(respuesta)
+//    const response = (await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=100&addRecipeInformation=true`)).data;
+//    res.status(200).json(response.results)
 };
 
--- PI-Food-main/api/src/controllers/getRecepiById.js
+++ PI-Food-main/api/src/controllers/getRecepiById.js
@@ -1,5 +1,10 @@
-
+require('dotenv').config();
+const { API_KEY} = process.env;
+const axios = require('axios');
 
 module.exports = async (req,res)=>{
-console.log(req)
+    const {idRecipe} = req.params;
+   const {results} = (await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=100&addRecipeInformation=true`)).data;
+ const recepiFinded =  results.filter(e=>e.id === Number(idRecipe));
+   res.status(200).json(recepiFinded);
 }
