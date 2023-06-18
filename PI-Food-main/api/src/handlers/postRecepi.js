0e65cebcdf8935c762990e601962580709657916
H3rC4 <hernanenriquecaballero@gmail.com>
2023-06-19 01:46:48 +0200

    todas las funcionalidad termiandaws faltan los diets from DB

--- PI-Food-main/api/src/handlers/postRecepi.js
+++ PI-Food-main/api/src/handlers/postRecepi.js
@@ -1,13 +1,21 @@
 const { Recipe } = require("../db");
+const { Diets } = require("../db");
 
 module.exports = async (req, res)=>{
+  const{name, image, resumen, health, paso, diets}= req.body;
   
   try {
-      const{name, image, resumen, health, paso, diets}= req.body;
       if(!name || !resumen || !health || !image || !paso || !diets  ){
         return  res.status(404).send('Faltan datos') 
       }
-      await Recipe.create({name, image, resumen, health, paso, diets})
+      const recetaCreda = await Recipe.create({name, image, resumen, health, paso, diets})
+      const findDiets = await Diets.findAll({
+        where: {
+          id: diets,
+        },
+      });
+    await recetaCreda.addDiets(findDiets);
+     
       return res.status(200).send('Tu receta se a creado satisfactoria mente');
     
   } catch (error) {
